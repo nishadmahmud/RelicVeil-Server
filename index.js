ebb4269a16a3d05abb20c70a1345909890cc885d
Nishad <mahmudnishad253@gmail.com>
2025-06-11 22:01:23 +0600

    Server start

---
+++ index.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient } = require('mongodb');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@ph-cluster.8kwdmtt.mongodb.net/?retryWrites=true&w=majority&appName=PH-Cluster`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    console.log("Connected to MongoDB");
+
+    const db = client.db("testDB");
+    const collection = db.collection("testCollection");
+
+    app.get("/", (req, res) => {
+      res.send("Server is running!");
+    });
+
+    // You can add more routes here...
+
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+run().catch(console.dir);
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
